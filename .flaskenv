6a6e30009845d307e0999f2d3bf3583a51cd55a4
Samuel Williams <samuelhwilliams@gmail.com>
2024-11-15 14:26:49 +0000

    Remove .env.development; fix .flaskenv for DB access

--- .flaskenv
+++ .flaskenv
@@ -2,3 +2,4 @@ FLASK_APP=app:application
 FLASK_ENV=development
 FLASK_RUN_PORT=5000
 FLASK_RUN_HOST=localhost
+DATABASE_URL=postgresql://postgres:password@localhost:5432/pre_award_stores
---
+++ db/migrations/versions/001_initial_migration.py
@@ -0,0 +1,24 @@
+"""Set up alembic migrations
+
+Revision ID: 001_initial_migration
+Revises: 
+Create Date: 2024-11-15 14:50:58.437872
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '001_initial_migration'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
