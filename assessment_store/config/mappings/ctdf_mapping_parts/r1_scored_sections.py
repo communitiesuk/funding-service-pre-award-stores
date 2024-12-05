039d4b107aeee09574f1c0001671ce7832caa300
srh-sloan <sarah.sloan@communities.gov.uk>
2024-12-05 13:51:26 +0000

    Giving CTDF a scored section

---
+++ assessment_store/config/mappings/ctdf_mapping_parts/r1_scored_sections.py
@@ -0,0 +1,30 @@
+# ruff: noqa: E501
+
+scored_sections = [
+    {
+        "id": "strategic_case",
+        "weighting": 1.0,
+        "name": "Organisation Details",
+        "sub_criteria": [
+            {
+                "id": "organisation_name",
+                "name": "Organisation name",
+                "themes": [
+                    {
+                        "id": "organisation_name",
+                        "name": "Organisation name",
+                        "answers": [
+                            {
+                                "field_id": "yptqZX",
+                                "form_name": "organisation-name-sample",
+                                "field_type": "textField",
+                                "presentation_type": "text",
+                                "question": "Organisation name",
+                            }
+                        ],
+                    }
+                ],
+            },
+        ],
+    }
+]
