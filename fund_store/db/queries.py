410c71b069b1863ff21d2d1ceaa55014cda0aa83
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2024-12-11 13:08:37 +0000

    updating script to check is ggis available otherwise add none

--- fund_store/db/queries.py
+++ fund_store/db/queries.py
@@ -277,7 +277,7 @@ def insert_fund_data(fund_config, commit: bool = True):
         "owner_organisation_name": fund_config["owner_organisation_name"],
         "owner_organisation_shortname": fund_config["owner_organisation_shortname"],
         "owner_organisation_logo_uri": fund_config["owner_organisation_logo_uri"],
-        "ggis_scheme_reference_number": fund_config["ggis_scheme_reference_number"],
+        "ggis_scheme_reference_number": fund_config["ggis_scheme_reference_number"] if 'ggis_scheme_reference_number' in fund_config else None,
         "funding_type": fund_config["funding_type"],
     }
 
