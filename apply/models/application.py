b5e5ad6f9083770987cf1ee2edd0d16a0fa8c45b
Albert Artur Kolozsvari <albertartk@gmail.com>
2024-12-19 15:31:55 +0000

    Adjust code

--- apply/models/application.py
+++ apply/models/application.py
@@ -79,14 +79,16 @@ class Application:
                         form_in_config["state"] = form_state
 
         for section in sections_config:
-            section["all_forms_complete"] = True
+            all_forms_complete = True
             for form in section["forms"]:
                 if form["state"] is None:
-                    section["all_forms_complete"] = False
+                    all_forms_complete = False
                     break
 
                 if ApplicationStatus.COMPLETED.name != form["state"]["status"]:
-                    section["all_forms_complete"] = False
+                    all_forms_complete = False
                     break
+
+            section["all_forms_complete"] = all_forms_complete
 
         return sections_config
