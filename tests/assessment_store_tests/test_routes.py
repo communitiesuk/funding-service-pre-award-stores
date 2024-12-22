063597e12220522844b41f30d425e985dcf0bc78
Samuel Williams <samuelhwilliams@gmail.com>
2024-12-22 12:09:29 +0000

    Rename app->app_data so it does not clash with app fixture name

--- tests/assessment_store_tests/test_routes.py
+++ tests/assessment_store_tests/test_routes.py
@@ -657,7 +657,7 @@ def test_get_all_applications_assigned_by_user(flask_test_client):
 
 COF_FUND_ID = "47aef2f5-3fcb-4d45-acb5-f0152b5f03c4"
 COF_ROUND_2_ID = "c603d114-5364-4474-a0c4-c41cbf4d3bbd"
-app = {"round_id": COF_ROUND_2_ID, "fund_id": COF_FUND_ID, "application_id": "app789"}
+app_data = {"round_id": COF_ROUND_2_ID, "fund_id": COF_FUND_ID, "application_id": "app789"}
 scoring_system = {"maximum_score": 5}
 sub_criteria_scores = {"sub1": 3, "sub2": 4, "sub3": 5}
 
@@ -694,7 +694,7 @@ def mock_get_scores(mocker):
 def test_calculate_overall_score_percentage_for_application(mocker, mock_get_scores, mock_get_scoring_system):
     mock_config = mocker.patch("assessment_store.api.routes.assessment_routes.Config")
     mock_config.ASSESSMENT_MAPPING_CONFIG = mapping_config
-    result = calculate_overall_score_percentage_for_application(app)
+    result = calculate_overall_score_percentage_for_application(app_data)
     expected_score = ((3 * 2 + 4 * 2 + 5 * 1) / (5 * 2 * 2 + 5 * 1 * 1)) * 100
     assert result == expected_score, "The calculated score did not match the expected score"
 
@@ -703,7 +703,7 @@ def test_with_no_sub_criteria_scores(mocker, mock_get_scores, mock_get_scoring_s
     mock_config = mocker.patch("assessment_store.api.routes.assessment_routes.Config")
     mock_config.ASSESSMENT_MAPPING_CONFIG = mapping_config
     mock_get_scores.return_value = {}
-    result = calculate_overall_score_percentage_for_application(app)
+    result = calculate_overall_score_percentage_for_application(app_data)
     assert result == 0, "The result should be 0 when there are no sub-criteria scores"
 
 
@@ -712,11 +712,11 @@ def test_with_invalid_application_id(mocker, mock_get_scores, mock_get_scoring_s
     mock_config.ASSESSMENT_MAPPING_CONFIG = mapping_config
     mock_get_scores.side_effect = KeyError("Invalid application ID")
     with pytest.raises(KeyError):
-        calculate_overall_score_percentage_for_application(app)
+        calculate_overall_score_percentage_for_application(app_data)
 
 
 def test_no_scored_criteria_exists(mocker, mock_get_scores, mock_get_scoring_system):
     mock_config = mocker.patch("assessment_store.api.routes.assessment_routes.Config")
     mock_config.ASSESSMENT_MAPPING_CONFIG = {f"{COF_FUND_ID}:{COF_ROUND_2_ID}": {"scored_criteria": []}}
-    result = calculate_overall_score_percentage_for_application(app)
+    result = calculate_overall_score_percentage_for_application(app_data)
     assert result is None, "The result should be 0 when there are no scored criteria"
