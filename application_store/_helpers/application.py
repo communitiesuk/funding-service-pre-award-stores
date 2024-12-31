f6bfd8aa532499ae6b71ab40aa2f2df6c7e52b8d
Samuel Williams <samuelhwilliams@gmail.com>
2024-12-31 07:32:55 +0000

    Send application submitted emails synchronously
    Switch from sending messages to SQS for the notification service to pick
    up, to sending emails in-request on application submission.
    
    This is because we aren't at a scale where the technical complexity of a
    queueing infrastructure does not give us enough benefit for the cost it
    brings.

--- application_store/_helpers/application.py
+++ application_store/_helpers/application.py
@@ -2,7 +2,6 @@ from operator import itemgetter
 
 from flask import current_app
 from fsd_utils import Decision
-from fsd_utils.config.notify_constants import NotifyConstants
 
 from application_store.config.key_report_mappings.mappings import (
     ROUND_ID_TO_KEY_REPORT_MAPPING,
@@ -11,8 +10,7 @@ from application_store.db.queries.application import create_qa_base64file
 from application_store.db.queries.reporting.queries import (
     map_application_key_fields,
 )
-from application_store.external_services.models.notification import Notification
-from config import Config
+from services.notify import NotificationError, get_notification_service
 
 
 def order_applications(applications, order_by, order_rev):
@@ -43,13 +41,20 @@ def send_submit_notification(
     application,
     round_data,
 ):
-    contents = {
-        NotifyConstants.APPLICATION_FIELD: create_qa_base64file(application_with_form_json_and_fund_name, True),
-        NotifyConstants.MAGIC_LINK_CONTACT_HELP_EMAIL_FIELD: round_data.contact_email,
-    }
-    del contents[NotifyConstants.APPLICATION_FIELD]["forms"]
+    application_data = create_qa_base64file(application_with_form_json_and_fund_name, True)
+    del application_data["forms"]
     full_name = account.full_name
 
+    questions = application_data.get("questions_file")
+    submission_date = application_data.get("date_submitted")
+    fund_name = application_data.get("fund_name")
+    fund_id = application_data.get("fund_id")
+    round_name = application_data.get("round_name")
+    application_reference = application_data.get("reference")
+    language = application_data.get("language")
+    prospectus_url = application_data.get("prospectus_url", "")
+
+    notification = None
     if eoi_results:
         eoi_decision = eoi_results["decision"]
         if not full_name:
@@ -64,11 +69,41 @@ def send_submit_notification(
                 return  # we don't send emails for a failure
 
             case Decision.PASS:
-                notify_template = Config.NOTIFY_TEMPLATE_EOI_PASS
+                try:
+                    notification = get_notification_service().send_eoi_pass_email(
+                        email_address=account.email,
+                        contact_name=full_name.title() if full_name else None,
+                        language=language,
+                        fund_id=fund_id,
+                        fund_name=fund_name,
+                        application_reference=application_reference,
+                        submission_date=submission_date,
+                        round_name=round_name,
+                        questions=questions,
+                        contact_help_email=round_data.contact_email,
+                    )
+                except NotificationError:
+                    current_app.logger.exception("Failed to send EOI Pass email")
 
             case Decision.PASS_WITH_CAVEATS:
-                notify_template = Config.NOTIFY_TEMPLATE_EOI_PASS_W_CAVEATS
-                contents[NotifyConstants.APPLICATION_CAVEATS] = eoi_results["caveats"]
+                caveats = eoi_results["caveats"]
+                try:
+                    notification = get_notification_service().send_eoi_pass_with_caveats_email(
+                        email_address=account.email,
+                        contact_name=full_name.title() if full_name else None,
+                        language=language,
+                        fund_id=fund_id,
+                        fund_name=fund_name,
+                        application_reference=application_reference,
+                        submission_date=submission_date,
+                        round_name=round_name,
+                        questions=questions,
+                        caveats=caveats,
+                        contact_help_email=round_data.contact_email,
+                    )
+                except NotificationError:
+                    current_app.logger.exception("Failed to send EOI Pass (with caveats) email")
+
             case _:
                 current_app.logger.error(
                     "Unknown eoi_decision [{eoi_decision}], unable to send submit notification",
@@ -76,16 +111,23 @@ def send_submit_notification(
                 )
                 return
     else:
-        notify_template = Config.NOTIFY_TEMPLATE_SUBMIT_APPLICATION
+        try:
+            notification = get_notification_service().send_submit_application_email(
+                email_address=account.email,
+                language=language,
+                fund_name=fund_name,
+                application_reference=application_reference,
+                submission_date=submission_date,
+                round_name=round_name,
+                questions=questions,
+                prospectus_url=prospectus_url,
+                contact_help_email=round_data.contact_email,
+            )
+        except NotificationError:
+            current_app.logger.exception("Failed to send submitted application email")
 
-    message_id = Notification.send(
-        notify_template,
-        account.email,
-        full_name.title() if full_name else None,
-        contents,
-    )
-
-    current_app.logger.info(
-        "Message added to the notification queue msg_id: [{message_id}]",
-        extra=dict(message_id=message_id),
-    )
+    if notification:
+        current_app.logger.info(
+            "Sent notification {notification_id} for application {application_reference}",
+            extra=dict(notification_id=notification.id, application_reference=application_reference),
+        )
