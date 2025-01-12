bdc0b8186bbb5cc89b370af181faba1aaae2684b
Samuel Williams <samuel.williams@communities.gov.uk>
2025-01-12 16:34:45 +0000

    flask admin backlink to service

--- proto/onboard/admin/__init__.py
+++ proto/onboard/admin/__init__.py
@@ -1,3 +1,5 @@
+from flask_admin.menu import MenuLink
+
 from proto.onboard.admin.entities import (
     DataStandardAdmin,
     FundAdmin,
@@ -13,3 +15,5 @@ def register_admin_views(flask_admin, db):
     flask_admin.add_view(DataStandardAdmin(db.session, category="Templates"))
     flask_admin.add_view(TemplateSectionAdmin(db.session, category="Templates"))
     flask_admin.add_view(TemplateQuestionAdmin(db.session, category="Templates"))
+
+    flask_admin.add_link(MenuLink(name="← Back to Onboard a grant", url="/"))
