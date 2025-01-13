0e0e445cce7750761fa44266cd54b983c592f6a0
srh-sloan <sarah.sloan@communities.gov.uk>
2025-01-13 12:18:11 +0000

    Basic template loading with latest govuk-frontend

---
+++ frontend/apply/routes.py
@@ -0,0 +1,10 @@
+from flask import render_template
+
+from pre_award.common.blueprints import Blueprint
+
+apply_bp = Blueprint("apply_routes", __name__, template_folder="templates")
+
+
+@apply_bp.route("/monolith")
+def landing_page():
+    return render_template("apply/landing-mono.html")
