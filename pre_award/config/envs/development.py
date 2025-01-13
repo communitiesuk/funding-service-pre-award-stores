1d0eaecad1958f0b2bfa5551542cd05e276c8145
srh-sloan <sarah.sloan@communities.gov.uk>
2025-01-13 09:20:45 +0000

    Running locally

--- pre_award/config/envs/development.py
+++ pre_award/config/envs/development.py
@@ -63,11 +63,11 @@ class DevelopmentConfig(Config):
 
     # RSA 256 KEYS
     if not hasattr(DefaultConfig, "RSA256_PUBLIC_KEY"):
-        _test_public_key_path = DefaultConfig.FLASK_ROOT + "/tests/keys/rsa256/public.pem"
+        _test_public_key_path = DefaultConfig.FLASK_ROOT + "/tests/pre_award/keys/rsa256/public.pem"
         with open(_test_public_key_path, mode="rb") as public_key_file:
             RSA256_PUBLIC_KEY = public_key_file.read()
     if not hasattr(DefaultConfig, "RSA256_PRIVATE_KEY"):
-        _test_private_key_path = DefaultConfig.FLASK_ROOT + "/tests/keys/rsa256/private.pem"
+        _test_private_key_path = DefaultConfig.FLASK_ROOT + "/tests/pre_award/keys/rsa256/private.pem"
         with open(_test_private_key_path, mode="rb") as private_key_file:
             RSA256_PRIVATE_KEY = private_key_file.read()
 
--- tasks.py
+++ tasks.py
@@ -1,8 +1,8 @@
 from invoke import Collection
 
-from account_store import tasks as account_tasks
-from assessment_store import tasks as assessment_tasks
-from common import tasks as common_tasks
+from pre_award.account_store import tasks as account_tasks
+from pre_award.assessment_store import tasks as assessment_tasks
+from pre_award.common import tasks as common_tasks
 
 namespace = Collection()
 namespace.add_collection(Collection.from_module(assessment_tasks), "assessment")
