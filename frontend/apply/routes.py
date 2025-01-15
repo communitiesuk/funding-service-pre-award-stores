136cb0353a1755094c02c5cef409b4051c463350
srh-sloan <sarah.sloan@communities.gov.uk>
2025-01-15 11:12:55 +0000

    welsh

--- frontend/apply/routes.py
+++ frontend/apply/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import abort, render_template
 
 from pre_award.common.blueprints import Blueprint
 from services.data.queries.fund_round_queries import get_fund_and_round
@@ -9,4 +9,6 @@ apply_bp = Blueprint("apply_routes", __name__, template_folder="templates")
 @apply_bp.route("/monolith/<fund_short_name>/<round_short_name>")
 def landing_page(fund_short_name: str, round_short_name: str):
     fund, round = get_fund_and_round(fund_short_name, round_short_name)
+    if not fund or not round:
+        return abort(404)
     return render_template("apply/landing-mono.html", fund=fund, round=round)
